b5e21559270b0d77f4346d125fc640868fd0de8f
David <davidsilvabrito91@gmail.com>
2026-09-04 07:26:21 -0300

    a

--- apps/frontend/src/app/robots.ts
+++ apps/frontend/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/ponto/', '/dashboard/', '/api/'],
     },
-    sitemap: 'https://gennesisconecta.com.br/sitemap.xml',
+    sitemap: 'https://www.gennesisconecta.com.br/sitemap.xml',
   };
 }
